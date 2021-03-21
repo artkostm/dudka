7cf765b16d5dfa47627bd944e621722931dac460
Artsiom Chuiko <artkostm@gmail.com>
2021-03-21 17:20:42 +0300

    Create example.scala
    add an example

---
+++ src/main/scala/dudka/example.scala
@@ -0,0 +1,33 @@
+package dudka
+
+import java.time.{LocalDate, LocalDateTime}
+
+
+object Example {
+
+
+  import com.petrofac.data.landing.rest.protocol.codec.cli._
+  import CliParserDerivation._
+
+  case class TestArgs(
+                       @doc("the very first cli param") first: String,
+                       @name.long("seconnnd") second: String,
+                       @name.short("t") third: String,
+                       test: Boolean,
+                       i: Int,
+                       ld: LocalDate,
+                       ldt: LocalDateTime,
+                       lng: Long,
+                       dbl: Double,
+                       opt: Option[String],
+                       dflt: Int = 45
+                     )
+
+  def main(args: Array[String]): Unit = {
+
+    cli[TestArgs](args).fold(e => new RuntimeException(e), cliArgs => {
+      println(cliArgs)
+      // any code here
+    })
+  }
+}
